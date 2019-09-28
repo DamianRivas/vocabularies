b8bfd39bff55a17282492a8438c822e130bd0a88
Wagner, David A (US 313D) <David.A.Wagner@jpl.nasa.gov>
2019-09-28 15:38:00 -0700

    initial commit of gh-pages branch

---
+++ publish.sh
@@ -0,0 +1,9 @@
+bikeshed spec opencaesar.io/foundation/analysis.bs
+bikeshed spec opencaesar.io/foundation/base.bs
+bikeshed spec opencaesar.io/foundation/mission.bs
+bikeshed spec opencaesar.io/foundation/project.bs
+bikeshed spec purl.org/dc/elements/1.1.bs
+bikeshed spec www.w3.org/1999/02/22-rdf-syntax-ns.bs
+bikeshed spec www.w3.org/2000/01/rdf-schema.bs
+bikeshed spec www.w3.org/2001/XMLSchema.bs
+bikeshed spec www.w3.org/2002/07/owl.bs
